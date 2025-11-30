b7cd87f6b8a524f4b2cf0e7da7ac7db058553f04
carbon718 <39200425+carbon718@users.noreply.github.com>
2025-11-30 13:57:57 +0100

    Endpoint QR code to worker Endpoint returns JSON with worker data generating Secret for QR code decoding QR code

--- backend/components/qrcode/qrcodeService.py
+++ backend/components/qrcode/qrcodeService.py
@@ -1,7 +1,10 @@
+from random import random
+
 from flask import request, send_file, Response
 from sqlalchemy import select
-
-
+import cv2
+import numpy as np
+import hashlib
 from backend.app import db
 
 def generateQRCode():
@@ -9,3 +12,53 @@ def generateQRCode():
 
 def validateQRCode():
     pass
+
+
+class QRCodeError(Exception):
+    """Base error class for QR codes"""
+    pass
+
+class MultipleCodesError(QRCodeError):
+    """Raised when more than one code is detected"""
+    pass
+
+class NoCodeFoundError(QRCodeError):
+    """Raised when no code is detected"""
+    pass
+
+def decodeQRImage(file_stream) -> str:
+    """
+    Input a file stream (bytes) and return decoded QR code data as string.
+    """
+    file_bytes = np.frombuffer(file_stream.read(), np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    if img is None:
+        raise ValueError("Nie udało się przetworzyć pliku jako obrazu.")
+    qr_detector = cv2.QRCodeDetector()
+    retval, decoded_info, points, straight_qrcode = qr_detector.detectAndDecodeMulti(img)
+    if retval and decoded_info is not None:
+        valid_codes = [code for code in decoded_info if code]
+        count = len(valid_codes)
+
+        if count == 1:
+            return valid_codes[0]
+        elif count > 1:
+            raise MultipleCodesError(f"Wykryto {count} kodów QR. Wymagany jest dokładnie jeden.")
+        else:
+            raise NoCodeFoundError("Wykryto wzorzec QR, ale nie udało się go odczytać.")
+
+    raise NoCodeFoundError("Nie wykryto kodu QR.")
+
+
+def genereteSecret(worker_id: int, name: str) -> str:
+    rand_value = str(random.randint(100000, 999999))
+    raw_string = f"{worker_id}:{name}:{rand_value}"
+    secret_hash = hashlib.sha256(raw_string.encode()).hexdigest()
+    return secret_hash
+
+def getWorkerByQRCodeSecret(secret: str):
+    from backend.database.models import Worker
+
+    stmt = select(Worker).where(Worker.secret == secret)
+    result = db.session.execute(stmt).scalar_one_or_none()
+    return result
